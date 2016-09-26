866784e7a8f4c083f3511580753fe60f7e31487a
lwygit <jyh120@outlook.com>
2016-09-27 01:21:00 +0800

    修改数据库服务器为localhost

--- wuan_console/src/application/config/database.php
+++ wuan_console/src/application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'dev.wuanlife.com',
+	'hostname' => 'localhost',
 	'port' => '3306',
 	'username' => 'root',
 	'password' => 'root',
