0779dc77b793d5da272e939f7059ddd994c1529f
aunhappy <1195417752@qq.com>
2017-04-02 15:19:59 +0800

    用户管理及登录完成

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -3,15 +3,69 @@ namespace app\index\controller;
 
 use app\index\model\User as UserModel;
 use think\Controller;
+use think\Request;
+use think\Session;
 
 class User extends Controller
 {
     // 获取用户数据列表并输出
-	public function index()
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+        if(empty($_GET)&&empty(Session::get()))
+        {
+            echo $this->fetch('index');exit;
+        }
+    }
+
+
+    public function index()
 	{
-	    // 分页输出列表 每页显示5条数据
-	    $list = UserModel::paginate(5);
-	    $this->assign('list',$list);
-	    return $this->fetch();
+        Session::clear();
+	    return $this->fetch('index');
+	}
+
+	public function login()
+	{
+		$user=new UserModel();
+		$data['email'] = Request::instance()->get('email');
+		$data['password'] = md5(Request::instance()->get('password'));
+		$re = $user->login($data);
+        //print_r($re);exit;
+        if(!empty($re)){
+            $rs = $re[0];
+            if(in_array($rs['auth'],['02','03'])) {
+				Session::set('auth', $rs['auth']);
+				Session::set('id', $rs['id']);
+                echo '登录成功';
+			}else{
+                echo '您不是管理员';
+            }
+
+        }else{
+            echo '邮箱或者密码错误！';
+        }
+
+	}
+
+	public function get_user()
+	{
+		$user=new UserModel();
+		$pn = Request::instance()->get('pn');
+		$list = $user->get_user($pn);
+		$this->assign('pn',$list['pn']);
+		$this->assign('data',$list['all_num']);
+		$this->assign('page_count',$list['page_count']);
+		$this->assign('list',$list['user']);
+		return $this->fetch('get_user');
+	}
+
+	public function re_psw()
+	{
+		$data['user_id'] = Request::instance()->get('user_id'); // 获取某个get变量
+		$data['password'] = md5('123456');
+		$user_model = new UserModel();
+		$user_model->re_psw($data);
+        echo '重置密码为123456成功';
 	}
 }
--- application/index/model/User.php
+++ application/index/model/User.php
@@ -1,10 +1,62 @@
 <?php
-	namespace app\index\model;
+namespace app\index\model;
+use think\Model;
+use think\DB;
 
-	use think\Model;
+class User extends Model
+{
+    public function get_user($pn){
+        $all_num = Db::table('user_base')->count();
+        $page_num     = 16;                                       //每页条数
+        $pageCount =ceil($all_num/$page_num);                //总页数
+        if ($pageCount == 0){
+            $pageCount =1;
+        }
+        if($pn > $pageCount){
+            $pn = $pageCount;
+        }
+        $pn         =empty($pn)?1:$pn;                    //当前页数
+        $pn         =(int)$pn;                              //安全强制转换
+        $limit_st     =($pn-1)*$page_num;                     //起始数
+        $start = date('Y-m-').'01 00:00:00';
+        $end = date('Y-m-t').' 23:59:59';
+        $sql = 'SELECT id AS user_id,nickname AS user_name,	email AS user_email,COUNT(post_base_id) as num '
+            .'FROM user_base '
+            .'LEFT JOIN post_detail ON user_base.id = post_detail.user_base_id '
+            ."AND post_detail.create_time >= :start "
+            ."AND post_detail.create_time <= :end "
+            .'AND post_detail.floor = 1 '
+            .'GROUP BY user_id '
+            .'ORDER BY num DESC,id DESC '
+            .'LIMIT :limit_st,:page_num';
+        $parms = ['start'=>$start,'end'=>$end,'limit_st'=>$limit_st,'page_num'=>$page_num];
+        $rs = [
+            'all_num' => $all_num,
+            'pn'      => $pn,
+            'user'    => Db::query($sql,$parms),
+            'page_count' => $pageCount
+        ];
+        return $rs;
+    }
+    public function re_psw($data)
+    {
+        $sql = 'update user_base set password = :password where id = :id ';
+        $parms = ['password'=>$data['password'],'id'=>$data['user_id']];
 
-	class User extends Model
-	{
-	    // 设置完整的数据表（包含前缀）
-	    protected $table = 'user_base';
-	}
+        return Db::query($sql,$parms);
+    }
+
+    public function login($data)
+    {
+        $sql = 'SELECT authorization AS auth,id '
+            .'FROM user_detail '
+            .'JOIN user_base ON user_base.id = user_detail.user_base_id '
+            .'WHERE email = :email '
+            .'AND password = :password ';
+        $parms = ['password'=>$data['password'],'email'=>$data['email']];
+
+        return Db::query($sql,$parms);
+    }
+
+
+}
