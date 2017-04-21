6096c00e033d7eb252b87d82c6699e8bd77e06c9
aunhappy <1195417752@qq.com>
2017-04-21 21:00:28 +0800

    查看用户当月发帖数

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,30 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+use think\Db;
+
+class Index extends Controller
 {
+
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        $list = $this->get_user_data();
+        $this->assign('list',$list);
+        return $this->fetch('user');
     }
+
+    public function get_user_data(){
+        $start = date('Y-m-').'01 00:00:00';
+        $end = date('Y-m-t').' 23:59:59';
+        $rs= \think\Db::name('user_base')
+            ->join('post_detail','user_base.id = post_detail.user_base_id AND post_detail.createTime >= :start AND post_detail.createTime <= :end AND post_detail.floor = 1','LEFT')
+            ->field('id AS user_id,nickname AS user_name,Email AS user_email,COUNT(post_base_id) AS num')
+            ->bind(['start'=>$start,'end'=>$end])
+            ->group('user_base.id')
+            ->order('num DESC,id DESC')
+            ->paginate(3);
+        return $rs;
+    }
+
 }
